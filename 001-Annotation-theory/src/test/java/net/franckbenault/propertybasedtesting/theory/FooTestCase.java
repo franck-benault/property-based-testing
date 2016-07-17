fda1d7a369941a336dbf4d110b031ec94bd6951e
franck-benault <franck-benault@orange.fr>
2016-07-17 14:42:12 +0200

    add dummy test

---
+++ 001-Annotation-theory/src/test/java/net/franckbenault/propertybasedtesting/theory/FooTestCase.java
@@ -0,0 +1,14 @@
+package net.franckbenault.propertybasedtesting.theory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FooTestCase {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
