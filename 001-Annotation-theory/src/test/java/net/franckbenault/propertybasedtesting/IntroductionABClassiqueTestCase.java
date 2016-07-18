4d2df4318fcbf82d0e7a147437f8001a7d61fc20
franck-benault <franck-benault@orange.fr>
2016-07-18 23:34:38 +0200

    complete classique example a b a+b

---
+++ 001-Annotation-theory/src/test/java/net/franckbenault/propertybasedtesting/IntroductionABClassiqueTestCase.java
@@ -0,0 +1,42 @@
+package net.franckbenault.propertybasedtesting;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Franck Benault
+ * For all a,b>0
+ * the following is true 
+ * a+b>a and a+b>b
+ *
+ */
+public class IntroductionABClassiqueTestCase {
+
+	/**
+	 * this test is an example
+	 */
+	@Test
+	public void a_plus_b_greater_than_a_and_greater_than_b_test() {
+		int a =2;
+		int b =3;
+		assertTrue(a+b > b);
+		assertTrue(a+b > a);
+	}
+	
+	@Test
+	public void a_plus_b_greater_than_a_and_greater_than_b_multiple_values_test() {
+		List<Integer> values = Arrays.asList(1,2,300,4000000);
+		for(Integer a: values) {
+			for(Integer b:values) {
+				assertTrue(a+b > b);
+				assertTrue(a+b > a);
+			}
+		}
+	}	
+
+}
