2901bfd260abd6bd8bfa483320d0e43a81ddbb52
franck-benault <franck-benault@orange.fr>
2016-07-24 00:01:10 +0200

    add theory with parameters with different type

--- 001-Annotation-theory/src/test/java/net/franckbenault/propertybasedtesting/string/StringTestCase.java
+++ 001-Annotation-theory/src/test/java/net/franckbenault/propertybasedtesting/string/StringTestCase.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Assume;
 import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
@@ -29,6 +30,10 @@ public class StringTestCase {
 	 @DataPoint
 	 public static String d = null;
 
+	 @DataPoints
+	 public static int[] ints() {
+		 return new int[] {1 ,10 , 123456}; 
+	 }
 	 
 	@Theory(nullsAccepted=false)
 	public void testConcat(String x, String y) {
@@ -42,5 +47,12 @@ public class StringTestCase {
 		Assume.assumeNotNull(x, y);
 		System.out.println("x="+x+" ,y="+y);
 		assertEquals(x.length()+y.length(),(x+y).length());
+	}
+	
+	@Theory
+	public void testConcatInts(String x, int y) {
+		Assume.assumeNotNull(x);
+		System.out.println("x="+x+" ,y="+y);
+		assertEquals(x.length()+(int) Math.log10(y)+1,(x+y).length());
 	}
 }
