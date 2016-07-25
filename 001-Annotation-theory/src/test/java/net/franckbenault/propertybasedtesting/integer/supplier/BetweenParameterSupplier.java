85fdf3a1892fd5f8f5a94af030ac09352d4b9b6f
franck-benault <franck-benault@orange.fr>
2016-07-25 21:10:04 +0200

    cleaning code import unless variable

--- 001-Annotation-theory/src/test/java/net/franckbenault/propertybasedtesting/integer/supplier/BetweenParameterSupplier.java
+++ 001-Annotation-theory/src/test/java/net/franckbenault/propertybasedtesting/integer/supplier/BetweenParameterSupplier.java
@@ -10,20 +10,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
-import static org.junit.experimental.theories.PotentialAssignment.forValue;
-
 public class BetweenParameterSupplier extends ParameterSupplier {
 
-	private static final String VALIDATION_ERROR_MESSAGE = "\"n\" must be non negative number! Was: %d";
 
 	// The annotation for input parameters:
 	@Retention(RetentionPolicy.RUNTIME)
 	@ParametersSuppliedBy(BetweenParameterSupplier.class)
 	public @interface Between {
-		/**
-		 * Number of Fibonacci numbers to return.
-		 */
+
 		int first();
 
 		int last();
