5468f8a52797f497af4ae92fb8e0f1d65d9d8390
franck-benault <franck-benault@orange.fr>
2016-07-29 00:29:09 +0200

    add example with integer

---
+++ 002-JUnit-Quickcheck/src/test/java/net/franckbenault/propertybasetesting/integer/IntegerTestCase.java
@@ -0,0 +1,23 @@
+package net.franckbenault.propertybasetesting.integer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.runner.RunWith;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+@RunWith(JUnitQuickcheck.class)
+public class IntegerTestCase {
+	
+	
+	@Property(trials=5)
+	public void testAplusBGreaterThanA(
+			@InRange(min = "1", max = "99")int a, 
+			@InRange(min = "1", max = "99")int b) {
+		System.out.println("a="+a+", b="+b);
+		assertTrue((a+b) >a);
+	}
+
+}
