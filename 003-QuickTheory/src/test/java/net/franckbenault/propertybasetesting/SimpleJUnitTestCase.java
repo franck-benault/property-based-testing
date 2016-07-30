2b0664ae49194156cd79cdf7908e543f45e97fb0
franck-benault <franck-benault@orange.fr>
2016-07-30 09:43:14 +0200

    add in simple junit test boolean

--- 003-QuickTheory/src/test/java/net/franckbenault/propertybasetesting/SimpleJUnitTestCase.java
+++ 003-QuickTheory/src/test/java/net/franckbenault/propertybasetesting/SimpleJUnitTestCase.java
@@ -16,12 +16,29 @@ public class SimpleJUnitTestCase {
 		return i + j > 0;
 	}
 
+	private boolean isAssociative(boolean b1, boolean b2 ) {
+		counter++;
+		System.out.println("counter "+counter+", b1="+b1+", b2="+b2);  
+		return (b1&&b2) == (b2&&b1);
+	}
+	
 	  @Test
-	  public void addingTwoPositiveIntegersAlwaysGivesAPositiveInteger(){
-	    qt()
+	  public void testAddingTwoPositiveIntegersAlwaysGivesAPositiveInteger(){
+	    counter=0;
+		 qt()
 	    .forAll(integers().from(1).upTo(3000000)
 	          , integers().from(1).upTo(3000000))
 	    .check((i,j) -> isSomePositive(i,j)); 
+	  }
+	  
+	  @Test
+	  public void testBooleanOrIsAssociative(){
+	    counter=0;
+		 qt()
+	    .forAll(booleans().all(),
+	    		booleans().all()
+	    		)
+	    .check((b1,b2) -> isAssociative(b1,b2)); 
 	  }
 
 }
