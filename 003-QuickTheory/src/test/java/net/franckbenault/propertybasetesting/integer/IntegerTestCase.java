181ac97998675b9e9c7d8f837f3c2f8e6651cfaf
franck-benault <franck-benault@orange.fr>
2016-07-31 00:34:26 +0200

    quickTheory add all negative using assuming

--- 003-QuickTheory/src/test/java/net/franckbenault/propertybasetesting/integer/IntegerTestCase.java
+++ 003-QuickTheory/src/test/java/net/franckbenault/propertybasetesting/integer/IntegerTestCase.java
@@ -22,5 +22,28 @@ public class IntegerTestCase {
 	    .forAll(integers().all())
 	    .check((i) -> printOK(i)); 
 	}
+	
+	/**
+	 * all positive
+	 */
+	@Test
+	public void testAllPositiveInteger() {
+		qt()
+		.withFixedSeed(0)
+	    .forAll(integers().allPositive())
+	    .check((i) -> printOK(i)); 
+	}
+	
+	/**
+	 * all positive
+	 */
+	@Test
+	public void testAllNegativeIntegerUsingAssuming() {
+		qt()
+		.withFixedSeed(0)
+	    .forAll(integers().all())
+	    .assuming(i -> i <0)
+	    .check((i) -> printOK(i)); 
+	}
 
 }
