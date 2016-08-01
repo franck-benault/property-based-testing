b6b95fc6d4ece614238eb1d3410922bd0d9575c7
franck-benault <franck-benault@orange.fr>
2016-08-01 23:35:35 +0200

    add example with booleans generation

---
+++ 003-QuickTheory/src/test/java/net/franckbenault/propertybasetesting/booleans/BooleanTestCase.java
@@ -0,0 +1,31 @@
+package net.franckbenault.propertybasetesting.booleans;
+
+import static org.junit.Assert.*;
+import static org.quicktheories.quicktheories.QuickTheory.qt;
+import static org.quicktheories.quicktheories.generators.SourceDSL.booleans;
+
+import org.junit.Test;
+
+public class BooleanTestCase {
+
+	private int counter;
+	
+	private boolean printOK(boolean b1, boolean b2) {
+		counter++;
+		System.out.println("counter="+counter+ " b1="+b1+" b2="+b2);
+		return (b1&&b2) == (b2&&b1);
+	}
+	
+	/**
+	 * Seed = the random generated values are always the same...
+	 */
+	@Test
+	public void testAllIntegerWithSeed() {
+		counter =0;
+		qt()
+		.withFixedSeed(0)
+	    .forAll(booleans().all(), booleans().all())
+	    .check((b1,b2) -> printOK(b1,b2)); 
+	}
+
+}
