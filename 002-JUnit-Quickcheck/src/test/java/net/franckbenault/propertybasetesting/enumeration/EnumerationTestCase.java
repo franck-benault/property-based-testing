41ea384d2cab45d25e562f25d5576d4acf2deab4
franck-benault <franck-benault@orange.fr>
2016-08-09 23:11:54 +0200

    JUnit-Quickcheck ordered generation of enumeration

--- 002-JUnit-Quickcheck/src/test/java/net/franckbenault/propertybasetesting/enumeration/EnumerationTestCase.java
+++ 002-JUnit-Quickcheck/src/test/java/net/franckbenault/propertybasetesting/enumeration/EnumerationTestCase.java
@@ -4,19 +4,24 @@ package net.franckbenault.propertybasetesting.enumeration;
 import org.junit.runner.RunWith;
 
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.ValuesOf;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
 @RunWith(JUnitQuickcheck.class)
 public class EnumerationTestCase {
 	
 	public static enum JDKS {
-		JDK5, JDK6, JDK7,
+		JDK5, JDK6, JDK7, JDK8,
 	}
 	
 	
-	@Property(trials=5)
-	public void testEnum(JDKS j) {
-		System.out.println("j="+j);
+	@Property(trials=8)
+	public void testEnumRandom(JDKS j) {
+		System.out.println("random j="+j);
 	}
 
+	@Property(trials=8)
+	public void testEnumOrdered(@ValuesOf JDKS j) {
+		System.out.println("ordered j="+j);
+	}
 }
